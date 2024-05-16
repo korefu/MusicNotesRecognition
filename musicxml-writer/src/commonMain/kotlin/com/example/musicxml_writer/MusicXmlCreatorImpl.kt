36e537ab6521a158bb60ad9fc3dcda8075194812
s.nalisin <s.nalisin@tinkoff.ru>
2024-05-16 21:22:26 +0300

    refactor MusicXmlCreatorImpl

--- musicxml-writer/src/commonMain/kotlin/com/example/musicxml_writer/MusicXmlCreatorImpl.kt
+++ musicxml-writer/src/commonMain/kotlin/com/example/musicxml_writer/MusicXmlCreatorImpl.kt
@@ -1,6 +1,7 @@
 package com.example.musicxml_writer
 
 import me.tatarka.inject.annotations.Inject
+import org.redundent.kotlin.xml.Node
 import org.redundent.kotlin.xml.PrintOptions
 import org.redundent.kotlin.xml.XmlVersion
 import org.redundent.kotlin.xml.xml
@@ -32,75 +33,87 @@ class MusicXmlCreatorImpl : MusicXmlCreator {
                     "measure" {
                         attribute("number", "${measure.id}")
                         if (index == 0) {
-                            "attributes" {
-                                "divisions" { -"8" }
-                                "key" {
-                                    "fifths" { -"$fifths" }
-                                }
-                                "time" {
-                                    "beats" { -"$beats" }
-                                    "beat-type" { -"$beatType" }
-                                }
-                                "clef" {
-                                    "sign" { -"G" }
-                                    "line" { -"2" }
-                                }
-                            }
-                            "direction" {
-                                attribute("placement", "above")
-                                "direction-type" {
-                                    "metronome" {
-                                        attributes(
-                                            "font-family" to "EngraverTextT",
-                                            "font-size" to "12",
-                                            "parentheses" to "no",
-                                        )
-                                        "beat-unit" { -"quarter" }
-                                        "per-minute" { -"$bpm" }
-                                    }
-                                }
-                            }
+                            setTonality(fifths = fifths, beats = beats, beatType = beatType)
+                            setTimeSignature(bpm = bpm)
                         }
                         measure.notes.forEach { note ->
-                            "note" {
-                                when (note) {
-                                    is Note.Melodic -> {
-                                        "pitch" {
-                                            "step" { -"${note.name}" }
-                                            when (note.accidental) {
-                                                Note.Accidental.SHARP -> "alter" { -"1" }
-                                                Note.Accidental.FLAT -> "alter" { -"-1" }
-                                                Note.Accidental.NATURAL, Note.Accidental.NONE -> Unit
-                                            }
-                                            "octave" { -"${note.octave}" }
-                                        }
-                                    }
-
-                                    is Note.Rest -> {
-                                        "rest" {}
-                                    }
-                                }
-                                "duration" { -"${note.duration.getDuration()}" }
-                                "type" { -note.duration.text }
-                                if (note.duration.isDotted) "dot" {}
-                                if (note is Note.Melodic) {
-                                    if (note.accidental != Note.Accidental.NONE) {
-                                        "accidental" { -note.accidental.name.lowercase() }
-                                    }
-                                    if (note.isStaccato) {
-                                        "notations" {
-                                            "articulations" {
-                                                "staccato" {}
-                                            }
-                                        }
-                                    }
-                                }
-                            }
+                            setNote(note = note)
                         }
                     }
                 }
             }
         }
         return musicXml.toString(printOptions = PrintOptions(singleLineTextElements = true, useSelfClosingTags = true))
+    }
+
+    private fun Node.setTimeSignature(bpm: Int): Node {
+        return "direction" {
+            attribute("placement", "above")
+            "direction-type" {
+                "metronome" {
+                    attributes(
+                        "font-family" to "EngraverTextT",
+                        "font-size" to "12",
+                        "parentheses" to "no",
+                    )
+                    "beat-unit" { -"quarter" }
+                    "per-minute" { -"$bpm" }
+                }
+            }
+        }
+    }
+
+    private fun Node.setTonality(fifths: Int, beats: Int, beatType: Int): Node {
+        return "attributes" {
+            "divisions" { -"8" }
+            "key" {
+                "fifths" { -"$fifths" }
+            }
+            "time" {
+                "beats" { -"$beats" }
+                "beat-type" { -"$beatType" }
+            }
+            "clef" {
+                "sign" { -"G" }
+                "line" { -"2" }
+            }
+        }
+    }
+
+    private fun Node.setNote(note: Note): Node {
+        return "note" {
+            when (note) {
+                is Note.Melodic -> {
+                    "pitch" {
+                        "step" { -"${note.name}" }
+                        when (note.accidental) {
+                            Note.Accidental.SHARP -> "alter" { -"1" }
+                            Note.Accidental.FLAT -> "alter" { -"-1" }
+                            Note.Accidental.NATURAL, Note.Accidental.NONE -> Unit
+                        }
+                        "octave" { -"${note.octave}" }
+                    }
+                }
+
+                is Note.Rest -> {
+                    "rest" {}
+                }
+            }
+            "duration" { -"${note.duration.getDuration()}" }
+            "type" { -note.duration.text }
+            if (note.duration.isDotted) "dot" {}
+            if (note is Note.Melodic) {
+                if (note.accidental != Note.Accidental.NONE) {
+                    "accidental" { -note.accidental.name.lowercase() }
+                }
+                if (note.isStaccato) {
+                    "notations" {
+                        "articulations" {
+                            "staccato" {}
+                        }
+                    }
+                }
+            }
+        }
     }
 }
